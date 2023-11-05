c80c8176c9d3989bb1592f8c78e1ea12ec1a5e6c
mcmarius <23401453+mcmarius@users.noreply.github.com>
2023-11-05 16:20:57 +0200

    Actually compile problematic code

--- Chestie.cpp
+++ Chestie.cpp
@@ -2,13 +2,14 @@
 #include <string>
 #include <sstream>
 
-std::string Chestie::f() const {
+/*std::string Chestie::f() const {
     std::ostringstream oss;
     oss << static_cast<int>('1');
     return oss.str();
-}
+}*/
 
 std::ostream& operator<<(std::ostream& os, const Chestie& chestie) {
-    os << chestie.f();
+    //os << chestie.f();
+    os << chestie.s;
     return os;
 }
