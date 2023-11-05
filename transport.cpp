9db543bcc52e0d1f81c55f934b2b508f27420ffb
mcmarius <23401453+mcmarius@users.noreply.github.com>
2023-11-05 16:54:36 +0200

    Copy one more class

---
+++ transport.cpp
@@ -0,0 +1,26 @@
+#include "./transport.h"
+
+
+TransportLayer::TransportLayer(NetworkLayer& layer, uint16_t _sourcePort, uint16_t _destinationPort): 
+NetworkLayer(layer), sourcePort(_sourcePort), destinationPort(_destinationPort) {
+
+}
+
+TransportLayer::TransportLayer(const TransportLayer& layer): 
+NetworkLayer(layer), sourcePort(layer.sourcePort), destinationPort(layer.destinationPort) {
+
+}
+
+uint16_t TransportLayer::getSourcePort() const {
+    return sourcePort;
+}
+
+uint16_t TransportLayer::getDestinationPort() const {
+    return destinationPort;
+}
+
+std::ostream& operator<<(std::ostream& os, const TransportLayer& other) {
+	os << other.getIPSource() << std::string(":") << std::to_string(other.getSourcePort()) << std::string("->")
+        << other.getIPDestination() << std::string(":") << std::to_string(other.getDestinationPort());
+	return os;
+}
